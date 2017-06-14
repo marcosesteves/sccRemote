b9dede1a682b6f5e1a442c6f3538e1e9aa525621
emarco <emarco@CINFO-0841.ctex.eb.mil.br>
2017-06-14 11:10:08 -0300

    Teste de Push 14/06/17

--- scc/src/main/java/br/eb/ctex/scc/model/Computador.java
+++ scc/src/main/java/br/eb/ctex/scc/model/Computador.java
@@ -18,6 +18,7 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
+// Classe Computador
 @Entity()
 public class Computador {
 	
